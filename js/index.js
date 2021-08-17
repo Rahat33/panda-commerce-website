157b74d1f627f2c4136a447a419b76c658207883
Rahat Al Amin <46551614+Rahat33@users.noreply.github.com>
2021-08-17 11:34:30 +0600

    testing

--- js/index.js
+++ js/index.js
@@ -10,3 +10,35 @@ for (const h1 of hColor) {
 
 
 // (task 3) --> background color adding in backpack section
+
+const backpackClass = document.getElementById('backpack');
+
+backpackClass.style.backgroundColor = 'tomato';
+
+
+// (task 4) --> adding border-redius to card
+
+const cards = document.getElementsByClassName('card');
+
+for (const card of cards) {
+    // console.log(card);
+    card.style.borderRadius = '30px';
+}
+
+// (task 5) --> adding eventhandler with button
+
+document.getElementById('button-addon2').addEventListener('click', function () {
+    console.log('Event Handler');
+})
+
+
+// (task 6) --> adding eventhandler with all button
+
+
+const test = document.getElementsByClassName('buyNow');
+
+for (const buyButton of test) {
+    buyButton.addEventListener('click', function (event) {
+        event.target.parentNode.removeChild(event.target);
+    })
+}
